7daf71069883cddf3820d4f6d2ae7d35fed2af93
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-06-20 01:00:42 -0400

    Added more buttons to the mobile navbar

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,10 +1,18 @@
 import MobileNavLink from "./MobileNavLink";
 import UserIcon from "@heroicons/react/solid/UserIcon";
+import MenuIcon from "@heroicons/react/solid/MenuIcon";
+import SearchIcon from "@heroicons/react/solid/SearchIcon";
+import { useState } from "react";
+
 export default function MobileNavbar() {
+	const [showMenuDialog, setShowMenuDialog] = useState(false);
+
 	const iconClasses = "h-6 w-6";
 	return (
-		<div className="fixed bottom-0 bg-white w-full border-t md:hidden py-2">
-			<MobileNavLink icon={<UserIcon className={iconClasses} />} text="Account" to="/app/account" />
+		<div className="fixed bottom-0 bg-white w-full border-t md:hidden py-1 flex">
+			<MobileNavLink icon={<SearchIcon className={iconClasses} />} />
+			<MobileNavLink icon={<MenuIcon className={iconClasses} />} />
+			<MobileNavLink icon={<UserIcon className={iconClasses} />} to="/app/account" />
 		</div>
 	);
 }
--- src/components/MobileNavLink.js
+++ src/components/MobileNavLink.js
@@ -1,24 +1,31 @@
 import { Link, useRouteMatch } from "react-router-dom";
 import PropTypes from "prop-types";
 
-export default function MobileNavLink({ text, to, icon }) {
+export default function MobileNavLink({ text, to, icon, onClick }) {
 	let isCurrentRoute = useRouteMatch({
 		path: to,
 	});
-
-	return (
-		<Link
-			to={to}
-			className={`flex flex-col justify-center items-center rounded font-semibold transition-all ${
-				isCurrentRoute ? "text-blue-700 " : "text-gray-500 "
-			}`}
-		>
-			<span>{icon}</span> <span className="text-sm">{text}</span>
-		</Link>
-	);
+	if (to) {
+		return (
+			<Link
+				to={to}
+				className={`flex flex-col flex-grow justify-center items-center py-2 rounded font-semibold transition-all ${
+					isCurrentRoute ? "text-blue-700 " : "text-gray-500 "
+				} hover:text-gray-700`}
+			>
+				<span>{icon}</span> <span className="text-sm">{text}</span>
+			</Link>
+		);
+	} else {
+		return (
+			<button className="focus:outline-none outline-none py-2 flex flex-grow flex-col justify-center items-center rounded font-semibold transition-all text-gray-500">
+				<span>{icon}</span>
+				<span className="text-sm">{text}</span>
+			</button>
+		);
+	}
 }
 
 MobileNavLink.propTypes = {
-	to: PropTypes.string.isRequired,
 	text: PropTypes.string.isRequired,
 };
