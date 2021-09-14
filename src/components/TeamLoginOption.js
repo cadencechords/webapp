f610dffb12c90008d037b96b26b09ec83b00405e
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-09-14 18:13:30 -0400

    Changed look of login team button

--- src/components/TeamLoginOption.js
+++ src/components/TeamLoginOption.js
@@ -1,16 +1,17 @@
-import Button from "./Button";
+import MobileMenuButton from "./buttons/MobileMenuButton";
 import ProfilePicture from "./ProfilePicture";
 
 export default function TeamLoginOption({ team, onLoginTeam }) {
 	return (
-		<div className="flex-between p-3 m-2 border-b last:border-0">
+		<MobileMenuButton
+			className="flex-between p-3 border-b last:border-0"
+			onClick={() => onLoginTeam(team.id)}
+			full
+		>
 			<div className="flex items-center">
 				<ProfilePicture url={team.image_url} size="sm" />
 				<span className="ml-4">{team.name}</span>
 			</div>
-			<Button color="blue" onClick={() => onLoginTeam(team.id)}>
-				Login
-			</Button>
-		</div>
+		</MobileMenuButton>
 	);
 }
