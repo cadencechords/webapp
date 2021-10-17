272df7ca8d230897c9b0d2f03d514b735e8bc5fb
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-10-16 23:52:05 -0400

    Swipe delta to 40

--- src/components/SongsCarousel.js
+++ src/components/SongsCarousel.js
@@ -60,7 +60,7 @@ export default function SongsCarousel({ songs, index, onIndexChange }) {
 				items={buildTemplates()}
 				onSlideChanged={(e) => onIndexChange(e.slide)}
 				activeIndex={index}
-				swipeDelta={50}
+				swipeDelta={40}
 			/>
 
 			<PerformSetlistSongAdjustmentsDrawer
