bdeb413030d0c05d242a9ddd2ac46506ae460c64
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-10-21 13:03:32 -0400

    Updated icons in app menu

--- src/components/mobile menus/AppMenu.js
+++ src/components/mobile menus/AppMenu.js
@@ -1,13 +1,13 @@
+import BinderIcon from "../../icons/BinderIcon";
 import CalendarIcon from "@heroicons/react/outline/CalendarIcon";
-import FolderOpenIcon from "@heroicons/react/outline/FolderOpenIcon";
+import DashboardIcon from "../../icons/DashboardIcon";
 import { Link } from "react-router-dom";
 import MobileMenuButton from "../buttons/MobileMenuButton";
 import MusicNoteIcon from "@heroicons/react/solid/MusicNoteIcon";
+import PlaylistIcon from "../../icons/PlaylistIcon";
 import StyledDialog from "../StyledDialog";
-import TemplateIcon from "@heroicons/react/solid/TemplateIcon";
 import UserGroupIcon from "@heroicons/react/solid/UserGroupIcon";
 import { VIEW_EVENTS } from "../../utils/constants";
-import ViewGridAddIcon from "@heroicons/react/outline/ViewGridAddIcon";
 import { selectCurrentMember } from "../../store/authSlice";
 import { selectCurrentSubscription } from "../../store/subscriptionSlice";
 import { useSelector } from "react-redux";
@@ -15,12 +15,15 @@ import { useSelector } from "react-redux";
 export default function AppMenu({ onCloseDialog, open }) {
 	const currentSubscription = useSelector(selectCurrentSubscription);
 	const currentMember = useSelector(selectCurrentMember);
+
+	const iconClasses = "mr-4 w-5 h-5";
+
 	return (
 		<StyledDialog onCloseDialog={onCloseDialog} open={open} title="Menu" fullscreen={false}>
 			<Link to="/">
 				<MobileMenuButton full onClick={onCloseDialog}>
 					<div className="flex items-center text-gray-700">
-						<TemplateIcon className="mr-4 w-5 h-5" />
+						<DashboardIcon className={iconClasses} />
 						Dashboard
 					</div>
 				</MobileMenuButton>
@@ -29,7 +32,7 @@ export default function AppMenu({ onCloseDialog, open }) {
 			<Link to="/binders">
 				<MobileMenuButton full onClick={onCloseDialog}>
 					<div className="flex items-center text-gray-700">
-						<FolderOpenIcon className="mr-4 w-5 h-5" />
+						<BinderIcon className={iconClasses} />
 						Binders
 					</div>
 				</MobileMenuButton>
@@ -38,7 +41,7 @@ export default function AppMenu({ onCloseDialog, open }) {
 			<Link to="/songs">
 				<MobileMenuButton full onClick={onCloseDialog}>
 					<div className="flex items-center text-gray-700">
-						<MusicNoteIcon className="mr-4 w-5 h-5" />
+						<MusicNoteIcon className={iconClasses} />
 						Songs
 					</div>
 				</MobileMenuButton>
@@ -47,7 +50,7 @@ export default function AppMenu({ onCloseDialog, open }) {
 			<Link to="/sets">
 				<MobileMenuButton full onClick={onCloseDialog}>
 					<div className="flex items-center text-gray-700">
-						<ViewGridAddIcon className="mr-4 w-5 h-5" />
+						<PlaylistIcon className={iconClasses} />
 						Sets
 					</div>
 				</MobileMenuButton>
@@ -56,7 +59,7 @@ export default function AppMenu({ onCloseDialog, open }) {
 			<Link to="/members">
 				<MobileMenuButton full onClick={onCloseDialog}>
 					<div className="flex items-center text-gray-700">
-						<UserGroupIcon className="mr-4 w-5 h-5" />
+						<UserGroupIcon className={iconClasses} />
 						Team members
 					</div>
 				</MobileMenuButton>
@@ -66,7 +69,7 @@ export default function AppMenu({ onCloseDialog, open }) {
 				<Link to="/calendar">
 					<MobileMenuButton full onClick={onCloseDialog}>
 						<div className="flex items-center text-gray-700">
-							<CalendarIcon className="mr-4 h-5" />
+							<CalendarIcon className={iconClasses} />
 							Calendar
 						</div>
 					</MobileMenuButton>
