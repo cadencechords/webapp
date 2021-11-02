636a93bc6ffd2d14bc518763d7c4fdb8f5c85134
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-11-02 16:35:50 -0400

    Changed search bar to be button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,22 @@
-import SearchIcon from "@heroicons/react/outline/SearchIcon";
-import { useState } from "react";
 import OpenInput from "./inputs/OpenInput";
 import SearchDialog from "./SearchDialog";
+import SearchIcon from "@heroicons/react/outline/SearchIcon";
+import { useState } from "react";
 
 export default function SearchBar() {
 	const [isSearching, setIsSearching] = useState(false);
 
+	console.log(isSearching);
 	return (
 		<div className="ml-5 flex items-center">
 			<SearchIcon className="h-4 w-4 text-gray-500" />
 			<span className="ml-4 w-60">
-				<OpenInput placeholder="Search library" onFocus={() => setIsSearching(true)} value="" />
+				<button
+					className="outline-none focus:outline-none text-gray-500 w-full text-left p-2"
+					onClick={() => setIsSearching(true)}
+				>
+					Search library
+				</button>
 			</span>
 
 			<SearchDialog open={isSearching} onCloseDialog={() => setIsSearching(false)} />
