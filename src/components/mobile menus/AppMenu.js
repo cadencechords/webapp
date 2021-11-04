2f77b65d639c0d2a12990c4fba24be1cc29b2189
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-11-03 23:12:46 -0400

    Added ability to switch teams from app menu

--- src/components/mobile menus/AppMenu.js
+++ src/components/mobile menus/AppMenu.js
@@ -1,3 +1,5 @@
+import { selectCurrentMember, selectCurrentTeam } from "../../store/authSlice";
+
 import BinderIcon from "../../icons/BinderIcon";
 import CalendarIcon from "@heroicons/react/outline/CalendarIcon";
 import DashboardIcon from "../../icons/DashboardIcon";
@@ -6,20 +8,25 @@ import MobileMenuButton from "../buttons/MobileMenuButton";
 import MusicNoteIcon from "@heroicons/react/solid/MusicNoteIcon";
 import PlaylistIcon from "../../icons/PlaylistIcon";
 import StyledDialog from "../StyledDialog";
+import SwitchHorizontalIcon from "@heroicons/react/outline/SwitchHorizontalIcon";
 import UserGroupIcon from "@heroicons/react/solid/UserGroupIcon";
 import { VIEW_EVENTS } from "../../utils/constants";
-import { selectCurrentMember } from "../../store/authSlice";
 import { selectCurrentSubscription } from "../../store/subscriptionSlice";
 import { useSelector } from "react-redux";
 
 export default function AppMenu({ onCloseDialog, open }) {
 	const currentSubscription = useSelector(selectCurrentSubscription);
 	const currentMember = useSelector(selectCurrentMember);
-
+	const currentTeam = useSelector(selectCurrentTeam);
 	const iconClasses = "mr-4 w-5 h-5";
 
 	return (
-		<StyledDialog onCloseDialog={onCloseDialog} open={open} title="Menu" fullscreen={false}>
+		<StyledDialog
+			onCloseDialog={onCloseDialog}
+			open={open}
+			title={currentTeam.name}
+			fullscreen={false}
+		>
 			<Link to="/">
 				<MobileMenuButton full onClick={onCloseDialog}>
 					<div className="flex items-center text-gray-700">
@@ -75,6 +82,14 @@ export default function AppMenu({ onCloseDialog, open }) {
 					</MobileMenuButton>
 				</Link>
 			)}
+			<Link to="/login/teams">
+				<MobileMenuButton full onClick={onCloseDialog}>
+					<div className="flex items-center text-gray-700">
+						<SwitchHorizontalIcon className={iconClasses} />
+						Switch teams
+					</div>
+				</MobileMenuButton>
+			</Link>
 		</StyledDialog>
 	);
 }
