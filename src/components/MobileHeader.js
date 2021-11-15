f9a17e4a11f7f8e9457b95b34431a0f388621bfe
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-11-14 23:31:22 -0500

    Fixed display issue

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -4,7 +4,7 @@ import PlusIcon from "@heroicons/react/outline/PlusCircleIcon";
 export default function MobileHeader({ title, className, onAdd, canAdd }) {
 	return (
 		<div
-			className={`bg-white py-3 px-1 border-b fixed left-0 top-0 right-0 ${className} font-semibold text-center`}
+			className={`bg-white z-30 py-3 px-1 border-b fixed left-0 top-0 right-0 ${className} font-semibold text-center`}
 		>
 			{title}
 
