2ca2f33cdae9bf454168801365ef80491e00309f
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-02-19 16:12:15 -0500

    Prevents team from being created without name

--- src/pages/CreateNewTeamPage.js
+++ src/pages/CreateNewTeamPage.js
@@ -12,70 +12,78 @@ import { useHistory } from "react-router";
 import useQuery from "../hooks/useQuery";
 
 export default function CreateNewTeamPage() {
-	const [teamName, setTeamIdName] = useState("");
-	const [selectedPlan, setSelectedPlan] = useState("Starter");
-	const [loading, setLoading] = useState(false);
-	const router = useHistory();
-	const dispatch = useDispatch();
-	const requestedPlan = useQuery().get("requested_plan");
+  const [teamName, setTeamIdName] = useState("");
+  const [selectedPlan, setSelectedPlan] = useState("Starter");
+  const [loading, setLoading] = useState(false);
+  const router = useHistory();
+  const dispatch = useDispatch();
+  const requestedPlan = useQuery().get("requested_plan");
 
-	useEffect(() => {
-		if (requestedPlan && (requestedPlan === "Starter" || requestedPlan === "Pro")) {
-			setSelectedPlan(requestedPlan);
-		} else {
-			setSelectedPlan("Starter");
-		}
-	}, [requestedPlan]);
+  useEffect(() => {
+    if (
+      requestedPlan &&
+      (requestedPlan === "Starter" || requestedPlan === "Pro")
+    ) {
+      setSelectedPlan(requestedPlan);
+    } else {
+      setSelectedPlan("Starter");
+    }
+  }, [requestedPlan]);
 
-	const handleCreate = async () => {
-		setLoading(!loading);
+  const handleCreate = async () => {
+    setLoading(!loading);
 
-		try {
-			let newTeam = { name: teamName, plan: selectedPlan };
-			let { data } = await TeamApi.createOne(newTeam);
-			dispatch(setTeamId(data.id));
-			router.push("/");
-		} catch (error) {
-			reportError(error);
-			setLoading(false);
-		}
-	};
+    try {
+      let newTeam = { name: teamName, plan: selectedPlan };
+      let { data } = await TeamApi.createOne(newTeam);
+      dispatch(setTeamId(data.id));
+      router.push("/");
+    } catch (error) {
+      reportError(error);
+      setLoading(false);
+    }
+  };
 
-	return (
-		<CenteredPage>
-			<div className="font-bold text-xl mb-6 text-center">Your New Team</div>
-			<TeamPlanOption
-				name="Starter"
-				onClick={setSelectedPlan}
-				selected={"Starter" === selectedPlan}
-				className="mb-2"
-				pricing="$0.00"
-				trialMessage="Always free"
-			/>
-			<TeamPlanOption
-				name="Pro"
-				onClick={setSelectedPlan}
-				selected={"Pro" === selectedPlan}
-				className="mb-8"
-				pricing={
-					<div>
-						$10.00<span className="text-sm"> / month</span>
-					</div>
-				}
-				trialMessage="30 Day Free Trial"
-			/>
-			<div className="mb-1 font-semibold text-left">Your team's name</div>
-			<OutlinedInput
-				placeholder="Name"
-				value={teamName}
-				onChange={(editedTeamName) => setTeamIdName(editedTeamName)}
-			/>
+  return (
+    <CenteredPage>
+      <div className="font-bold text-xl mb-6 text-center">Your New Team</div>
+      <TeamPlanOption
+        name="Starter"
+        onClick={setSelectedPlan}
+        selected={"Starter" === selectedPlan}
+        className="mb-2"
+        pricing="$0.00"
+        trialMessage="Always free"
+      />
+      <TeamPlanOption
+        name="Pro"
+        onClick={setSelectedPlan}
+        selected={"Pro" === selectedPlan}
+        className="mb-8"
+        pricing={
+          <div>
+            $10.00<span className="text-sm"> / month</span>
+          </div>
+        }
+        trialMessage="30 Day Free Trial"
+      />
+      <div className="mb-1 font-semibold text-left">Your team's name</div>
+      <OutlinedInput
+        placeholder="Name"
+        value={teamName}
+        onChange={(editedTeamName) => setTeamIdName(editedTeamName)}
+      />
 
-			<div className="mt-6">
-				<Button full loading={loading} onClick={handleCreate}>
-					Create
-				</Button>
-			</div>
-		</CenteredPage>
-	);
+      <div className="mt-6">
+        <Button
+          full
+          loading={loading}
+          onClick={handleCreate}
+          disabled={!teamName}
+        >
+          Create
+        </Button>
+      </div>
+    </CenteredPage>
+  );
 }
