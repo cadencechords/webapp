2d9ce95aa98f135919ab38585555c91988815bba
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-05-22 19:59:43 -0400

    Added alert to import sources page

--- src/pages/SongImportSourcesIndexPage.js
+++ src/pages/SongImportSourcesIndexPage.js
@@ -2,6 +2,7 @@ import Button from "../components/Button";
 import DocumentAddIcon from "@heroicons/react/outline/DocumentAddIcon";
 import OnsongLogo from "../images/onsong.svg";
 import PlanningCenterButton from "../components/buttons/PlanningCenterButton";
+import Alert from "../components/Alert";
 
 export default function SongImportSourcesIndexPage() {
   return (
@@ -9,6 +10,9 @@ export default function SongImportSourcesIndexPage() {
       <div className="font-bold text-2xl text-center py-4">Import Songs</div>
 
       <div className="grid grid-cols-1 w-5/6  md:w-2/3 xl:w-1/2 mx-auto gap-y-4">
+        <Alert color="yellow" className="my-4">
+          Importing is not available in the playground
+        </Alert>
         <button
           disabled
           style={{ backgroundColor: "#1a1a1c", height: "40px" }}
