fdc65ee3d883f9b9c259d8e002fbc992b3f8e609
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-06-22 21:11:48 -0400

    Added environment to sentry

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-import "./index.css";
+import './index.css';
 
-import * as Sentry from "@sentry/react";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import * as Sentry from '@sentry/react';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import App from "./App";
-import { Integrations } from "@sentry/tracing";
-import { Provider } from "react-redux";
-import React from "react";
-import ReactDOM from "react-dom";
-import store from "./store/store";
+import App from './App';
+import { Integrations } from '@sentry/tracing';
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store from './store/store';
 
 Sentry.init({
-	dsn: process.env.REACT_APP_SENTRY_URL,
-	integrations: [new Integrations.BrowserTracing()],
-	tracesSampleRate: 1.0,
+  dsn: process.env.REACT_APP_SENTRY_URL,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+  environment: process.env.REACT_APP_SENTRY_ENVIRONMENT,
 });
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 serviceWorkerRegistration.unregister();
