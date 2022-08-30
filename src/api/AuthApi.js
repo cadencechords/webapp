01cac2f8a9f39b40202223b9b8d6d4178edb2aed
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-08-30 16:29:46 -0400

    Provide name when signing up

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -1,39 +1,53 @@
-import axios from "axios";
+import axios from 'axios';
 
 const AUTH_URL = `${process.env.REACT_APP_API_URL}/auth`;
 const WEB_APP_URL = process.env.REACT_APP_URL;
 
 export default class AuthApi {
-	static signUp(email, password, passwordConfirmation) {
-		return axios.post(process.env.REACT_APP_API_URL + "/auth", {
-			email,
-			password,
-			password_confirmation: passwordConfirmation,
-		});
-	}
+  static signUp({
+    email,
+    password,
+    passwordConfirmation,
+    firstName,
+    lastName,
+  }) {
+    return axios.post(process.env.REACT_APP_API_URL + '/auth', {
+      email,
+      password,
+      password_confirmation: passwordConfirmation,
+      first_name: firstName,
+      last_name: lastName,
+    });
+  }
 
-	static login(email, password) {
-		return axios.post(process.env.REACT_APP_API_URL + "/auth/sign_in", { email, password });
-	}
+  static login(email, password) {
+    return axios.post(process.env.REACT_APP_API_URL + '/auth/sign_in', {
+      email,
+      password,
+    });
+  }
 
-	static sendResetPasswordInstructions(email) {
-		return axios.post(`${AUTH_URL}/password`, {
-			email,
-			redirect_url: `${WEB_APP_URL}/reset_password`,
-		});
-	}
+  static sendResetPasswordInstructions(email) {
+    return axios.post(`${AUTH_URL}/password`, {
+      email,
+      redirect_url: `${WEB_APP_URL}/reset_password`,
+    });
+  }
 
-	static resetPassword(config) {
-		return axios.put(
-			`${AUTH_URL}/password`,
-			{ password: config.password, password_confirmation: config.passwordConfirmation },
-			{
-				headers: {
-					"access-token": config["access-token"],
-					uid: config.uid,
-					client: config.client,
-				},
-			}
-		);
-	}
+  static resetPassword(config) {
+    return axios.put(
+      `${AUTH_URL}/password`,
+      {
+        password: config.password,
+        password_confirmation: config.passwordConfirmation,
+      },
+      {
+        headers: {
+          'access-token': config['access-token'],
+          uid: config.uid,
+          client: config.client,
+        },
+      }
+    );
+  }
 }
--- src/pages/InvitationSignUpPage.js
+++ src/pages/InvitationSignUpPage.js
@@ -1,125 +1,169 @@
-import { setAuth, setTeamId } from "../store/authSlice";
-import { useEffect, useState } from "react";
+import { setAuth, setTeamId } from '../store/authSlice';
+import { useEffect, useState } from 'react';
 
-import Alert from "../components/Alert";
-import Button from "../components/Button";
-import CenteredPage from "../components/CenteredPage";
-import InvitationApi from "../api/InvitationApi";
-import OutlinedInput from "../components/inputs/OutlinedInput";
-import PasswordRequirements from "../components/PasswordRequirements";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router";
-import { useQuery } from "./ClaimInvitationPage";
+import Alert from '../components/Alert';
+import Button from '../components/Button';
+import CenteredPage from '../components/CenteredPage';
+import InvitationApi from '../api/InvitationApi';
+import OutlinedInput from '../components/inputs/OutlinedInput';
+import PasswordRequirements from '../components/PasswordRequirements';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { useQuery } from './ClaimInvitationPage';
 
 export default function InvitationSignUpPage() {
-	const [password, setPassword] = useState("");
-	const [passwordConfirmation, setPasswordConfirmation] = useState("");
-	const [isPasswordFocused, setIsPasswordFocused] = useState(false);
-	const [isLongEnough, setIsLongEnough] = useState(false);
-	const [isUncommon, setIsUncommon] = useState(false);
-	const [alertMessage, setAlertMessage] = useState(null);
-	const [loading, setLoading] = useState(false);
-	const dispatch = useDispatch();
-	const router = useHistory();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const [isLongEnough, setIsLongEnough] = useState(false);
+  const [isUncommon, setIsUncommon] = useState(false);
+  const [alertMessage, setAlertMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const router = useHistory();
 
-	const MIN_PASSWORD_LENGTH = 8;
-	const token = useQuery().get("token");
+  const MIN_PASSWORD_LENGTH = 8;
+  const token = useQuery().get('token');
 
-	const canSignUp = isUncommon && isLongEnough && token && password === passwordConfirmation;
+  const canSignUp =
+    isUncommon &&
+    isLongEnough &&
+    token &&
+    password === passwordConfirmation &&
+    firstName &&
+    lastName;
 
-	useEffect(() => {
-		document.title = "Sign Up";
-		const script = document.createElement("script");
+  useEffect(() => {
+    document.title = 'Sign Up';
+    const script = document.createElement('script');
 
-		script.src = process.env.REACT_APP_URL + "/scripts/passwords.js";
-		script.async = true;
+    script.src = process.env.REACT_APP_URL + '/scripts/passwords.js';
+    script.async = true;
 
-		document.body.appendChild(script);
+    document.body.appendChild(script);
 
-		return () => {
-			document.body.removeChild(script);
-		};
-	}, []);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
-	const handlePasswordChange = (passwordValue) => {
-		setPassword(passwordValue);
+  const handlePasswordChange = passwordValue => {
+    setPassword(passwordValue);
 
-		setIsLongEnough(passwordValue.length >= MIN_PASSWORD_LENGTH);
+    setIsLongEnough(passwordValue.length >= MIN_PASSWORD_LENGTH);
 
-		let { score } = window.zxcvbn(passwordValue);
+    let { score } = window.zxcvbn(passwordValue);
 
-		setIsUncommon(score >= 3);
-	};
+    setIsUncommon(score >= 3);
+  };
 
-	const handleSignUp = async () => {
-		setLoading(true);
-		try {
-			let result = await InvitationApi.signUpThroughToken(token, password, passwordConfirmation);
-			let accessToken = result.headers["access-token"];
-			let client = result.headers["client"];
-			let uid = result.headers["uid"];
-			dispatch(setAuth({ accessToken, client, uid }));
-			dispatch(setTeamId(result.data.team_id));
+  const handleSignUp = async () => {
+    setLoading(true);
+    try {
+      let result = await InvitationApi.signUpThroughToken({
+        token,
+        password,
+        passwordConfirmation,
+        firstName,
+        lastName,
+      });
+      let accessToken = result.headers['access-token'];
+      let client = result.headers['client'];
+      let uid = result.headers['uid'];
+      dispatch(setAuth({ accessToken, client, uid }));
+      dispatch(setTeamId(result.data.team_id));
 
-			router.push("/");
-		} catch (error) {
-			setAlertMessage(error?.response?.data?.message);
-			setLoading(false);
-		}
-	};
+      router.push('/');
+    } catch (error) {
+      setAlertMessage(error?.response?.data?.message);
+      setLoading(false);
+    }
+  };
 
-	if (token) {
-		return (
-			<CenteredPage>
-				<h1 className="font-bold text-3xl text-center mb-8">
-					You're almost there! <br /> Create a password for your new account
-				</h1>
+  if (token) {
+    return (
+      <CenteredPage>
+        <h1 className="mb-8 text-3xl font-bold text-center">
+          You're almost there! <br /> Create a password for your new account
+        </h1>
 
-				<div className="mb-2">Password</div>
-				<div className="mb-4">
-					<OutlinedInput
-						value={password}
-						placeholder="password"
-						type="password"
-						onFocus={() => setIsPasswordFocused(true)}
-						onChange={handlePasswordChange}
-					/>
+        <div className="flex items-center mb-4">
+          <div className="w-full pr-2">
+            <div className="mb-2">First name</div>
+            <OutlinedInput
+              value={firstName}
+              onChange={setFirstName}
+              placeholder="first name"
+            />
+          </div>
+          <div className="w-full pl-2">
+            <div className="mb-2">Last name</div>
+            <OutlinedInput
+              value={lastName}
+              onChange={setLastName}
+              placeholder="last name"
+            />
+          </div>
+        </div>
+        <div className="mb-2">Password</div>
+        <div className="mb-4">
+          <OutlinedInput
+            value={password}
+            placeholder="password"
+            type="password"
+            onFocus={() => setIsPasswordFocused(true)}
+            onChange={handlePasswordChange}
+          />
 
-					{isPasswordFocused && (
-						<div className="mt-4">
-							<PasswordRequirements isUncommon={isUncommon} isLongEnough={isLongEnough} />
-						</div>
-					)}
-				</div>
+          {isPasswordFocused && (
+            <div className="mt-4">
+              <PasswordRequirements
+                isUncommon={isUncommon}
+                isLongEnough={isLongEnough}
+              />
+            </div>
+          )}
+        </div>
 
-				<div className="mb-2">Confirm Password</div>
-				<div className="mb-4">
-					<OutlinedInput
-						value={passwordConfirmation}
-						placeholder="enter your password again"
-						type="password"
-						onChange={setPasswordConfirmation}
-					/>
-				</div>
+        <div className="mb-2">Confirm Password</div>
+        <div className="mb-4">
+          <OutlinedInput
+            value={passwordConfirmation}
+            placeholder="enter your password again"
+            type="password"
+            onChange={setPasswordConfirmation}
+          />
+        </div>
 
-				{alertMessage && (
-					<div className="mb-6">
-						<Alert color="red" dismissable onDismiss={() => setAlertMessage(null)}>
-							{alertMessage}
-						</Alert>
-					</div>
-				)}
+        {alertMessage && (
+          <div className="mb-6">
+            <Alert
+              color="red"
+              dismissable
+              onDismiss={() => setAlertMessage(null)}
+            >
+              {alertMessage}
+            </Alert>
+          </div>
+        )}
 
-				<Button full disabled={!canSignUp} loading={loading} onClick={handleSignUp}>
-					Sign Up
-				</Button>
-			</CenteredPage>
-		);
-	} else {
-		return (
-			<CenteredPage>
-				<Alert color="red">Invalid invitation</Alert>
-			</CenteredPage>
-		);
-	}
+        <Button
+          full
+          disabled={!canSignUp}
+          loading={loading}
+          onClick={handleSignUp}
+        >
+          Sign Up
+        </Button>
+      </CenteredPage>
+    );
+  } else {
+    return (
+      <CenteredPage>
+        <Alert color="red">Invalid invitation</Alert>
+      </CenteredPage>
+    );
+  }
 }
