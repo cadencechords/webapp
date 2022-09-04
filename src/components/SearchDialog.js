0f307d5526f1fa1f0dec4d6a8b87fe5afa46ffa9
Andrew Sidorchuk <andrew.sidorchuk@lifeway.com>
2022-09-03 21:03:21 -0400

    Added highlight feature when searching

--- src/components/SearchDialog.js
+++ src/components/SearchDialog.js
@@ -1,70 +1,74 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState } from 'react';
 
-import BinderApi from "../api/BinderApi";
-import OpenInput from "./inputs/OpenInput";
-import SearchResults from "./SearchResults";
-import SetlistApi from "../api/SetlistApi";
-import SongApi from "../api/SongApi";
-import StyledDialog from "./StyledDialog";
-import _ from "lodash";
-import { reportError } from "../utils/error";
+import BinderApi from '../api/BinderApi';
+import OpenInput from './inputs/OpenInput';
+import SearchResults from './SearchResults';
+import SetlistApi from '../api/SetlistApi';
+import SongApi from '../api/SongApi';
+import StyledDialog from './StyledDialog';
+import _ from 'lodash';
+import { reportError } from '../utils/error';
 
 export default function SearchDialog({ open, onCloseDialog }) {
-	const [searchQuery, setSearchQuery] = useState("");
-	const [searchResults, setSearchResults] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState(null);
 
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	const debounce = useCallback(
-		_.debounce(async (nameToSearchFor) => {
-			if (nameToSearchFor && nameToSearchFor !== "") {
-				let results = { binders: [], songs: [], setlists: [] };
-				try {
-					let bindersResponse = await BinderApi.search(nameToSearchFor);
-					results.binders = bindersResponse.data;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debounce = useCallback(
+    _.debounce(async nameToSearchFor => {
+      if (nameToSearchFor && nameToSearchFor !== '') {
+        let results = { binders: [], songs: [], setlists: [] };
+        try {
+          let bindersResponse = await BinderApi.search(nameToSearchFor);
+          results.binders = bindersResponse.data;
 
-					let songsResponse = await SongApi.search(nameToSearchFor);
-					results.songs = songsResponse.data;
+          let songsResponse = await SongApi.search(nameToSearchFor);
+          results.songs = songsResponse.data;
 
-					let setlistsResponse = await SetlistApi.search(nameToSearchFor);
-					results.setlists = setlistsResponse.data;
+          let setlistsResponse = await SetlistApi.search(nameToSearchFor);
+          results.setlists = setlistsResponse.data;
 
-					setSearchResults(results);
-				} catch (error) {
-					reportError(error);
-				}
-			}
-		}, 300),
-		[]
-	);
+          setSearchResults(results);
+        } catch (error) {
+          reportError(error);
+        }
+      }
+    }, 300),
+    []
+  );
 
-	const handleSearchQueryChange = (newQuery) => {
-		setSearchQuery(newQuery);
-		debounce(newQuery);
-	};
+  const handleSearchQueryChange = newQuery => {
+    setSearchQuery(newQuery);
+    debounce(newQuery);
+  };
 
-	const handleCloseDialog = () => {
-		setSearchQuery("");
-		setSearchResults(null);
-		onCloseDialog();
-	};
+  const handleCloseDialog = () => {
+    setSearchQuery('');
+    setSearchResults(null);
+    onCloseDialog();
+  };
 
-	return (
-		<StyledDialog
-			open={open}
-			onCloseDialog={handleCloseDialog}
-			borderedTop={false}
-			showClose={false}
-		>
-			<div className="border-b dark:border-dark-gray-400 pb-4">
-				<OpenInput
-					placeholder="Search for binders, songs or sets"
-					onChange={handleSearchQueryChange}
-					value={searchQuery}
-					autoFocus={true}
-				/>
-			</div>
+  return (
+    <StyledDialog
+      open={open}
+      onCloseDialog={handleCloseDialog}
+      borderedTop={false}
+      showClose={false}
+    >
+      <div className="pb-4 border-b dark:border-dark-gray-400">
+        <OpenInput
+          placeholder="Search for binders, songs or sets"
+          onChange={handleSearchQueryChange}
+          value={searchQuery}
+          autoFocus={true}
+        />
+      </div>
 
-			<SearchResults results={searchResults} onCloseDialog={handleCloseDialog} />
-		</StyledDialog>
-	);
+      <SearchResults
+        searchQuery={searchQuery}
+        results={searchResults}
+        onCloseDialog={handleCloseDialog}
+      />
+    </StyledDialog>
+  );
 }
