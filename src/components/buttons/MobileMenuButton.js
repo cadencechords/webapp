3219828efb2bb7298b68632957bdac1e3e631930
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2023-01-12 23:04:34 -0500

    Updated the mobile menu

--- src/components/buttons/MobileMenuButton.js
+++ src/components/buttons/MobileMenuButton.js
@@ -1,40 +1,41 @@
-import { TEXT_COLORS } from "../Button";
+import { TEXT_COLORS } from '../Button';
 
 export default function MobileMenuButton({
-	children,
-	onClick,
-	full,
-	color,
-	disabled,
-	className,
-	size,
+  children,
+  onClick,
+  full,
+  color,
+  disabled,
+  className,
+  size,
 }) {
-	let classes =
-		" font-semibold outline-none focus:outline-none text-sm transition-colors whitespace-nowrap overflow-hidden overflow-ellipsis";
-	let widthClasses = full ? " w-full " : "";
-	let colorClasses = disabled
-		? " text-gray-600 dark:text-dark-gray-200 cursor-default "
-		: ` ${TEXT_COLORS[color]} hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-dark-gray-600 dark:focus:bg-dark-gray-600 `;
+  let classes =
+    ' font-semibold outline-none focus:outline-none text-sm transition-colors whitespace-nowrap overflow-hidden overflow-ellipsis';
+  let widthClasses = full ? ' w-full ' : '';
+  let colorClasses = disabled
+    ? ' text-gray-600 dark:text-dark-gray-200 cursor-default '
+    : ` ${TEXT_COLORS[color]} hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-dark-gray-600 dark:focus:bg-dark-gray-600 `;
 
-	classes += widthClasses;
-	classes += colorClasses;
-	classes += SIZES[size];
-	classes += ` ${className}`;
-	return (
-		<button onClick={onClick} className={classes} disabled={disabled}>
-			{children}
-		</button>
-	);
+  classes += widthClasses;
+  classes += colorClasses;
+  classes += SIZES[size];
+  classes += ` ${className}`;
+  return (
+    <button onClick={onClick} className={classes} disabled={disabled}>
+      {children}
+    </button>
+  );
 }
 
 MobileMenuButton.defaultProps = {
-	color: "black",
-	disabled: false,
-	size: "md",
+  color: 'black',
+  disabled: false,
+  size: 'md',
 };
 
 const SIZES = {
-	xs: "py-1 px-4",
-	sm: "py-2 px-5",
-	md: "py-3 px-6",
+  xs: 'py-1 px-4',
+  sm: 'py-2 px-5',
+  md: 'py-3 px-6',
+  none: 'py-3 px-1',
 };
--- src/components/mobile menus/AppMenu.js
+++ src/components/mobile menus/AppMenu.js
@@ -1,95 +1,128 @@
-import { selectCurrentMember, selectCurrentTeam } from "../../store/authSlice";
+import { selectCurrentMember, selectCurrentTeam } from '../../store/authSlice';
 
-import BinderIcon from "../../icons/BinderIcon";
-import CalendarIcon from "@heroicons/react/outline/CalendarIcon";
-import DashboardIcon from "../../icons/DashboardIcon";
-import { Link } from "react-router-dom";
-import MobileMenuButton from "../buttons/MobileMenuButton";
-import MusicNoteIcon from "@heroicons/react/solid/MusicNoteIcon";
-import PlaylistIcon from "../../icons/PlaylistIcon";
-import StyledDialog from "../StyledDialog";
-import SwitchHorizontalIcon from "@heroicons/react/outline/SwitchHorizontalIcon";
-import UserGroupIcon from "@heroicons/react/solid/UserGroupIcon";
-import { VIEW_EVENTS } from "../../utils/constants";
-import { selectCurrentSubscription } from "../../store/subscriptionSlice";
-import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+import MobileMenuButton from '../buttons/MobileMenuButton';
+import StyledDialog from '../StyledDialog';
+import { MANAGE_BILLING, VIEW_EVENTS, VIEW_ROLES } from '../../utils/constants';
+import { selectCurrentSubscription } from '../../store/subscriptionSlice';
+import { useSelector } from 'react-redux';
+import BinderIcon from '../../icons/BinderIcon';
+import CalendarIcon from '@heroicons/react/outline/CalendarIcon';
+import DashboardIcon from '../../icons/DashboardIcon';
+import MusicNoteIcon from '@heroicons/react/solid/MusicNoteIcon';
+import PlaylistIcon from '../../icons/PlaylistIcon';
+import SwitchHorizontalIcon from '@heroicons/react/outline/SwitchHorizontalIcon';
+import UserGroupIcon from '@heroicons/react/solid/UserGroupIcon';
+import CreditCardIcon from '@heroicons/react/outline/CreditCardIcon';
+import LockClosedIcon from '@heroicons/react/outline/LockClosedIcon';
 
 export default function AppMenu({ onCloseDialog, open }) {
-	const currentSubscription = useSelector(selectCurrentSubscription);
-	const currentMember = useSelector(selectCurrentMember);
-	const currentTeam = useSelector(selectCurrentTeam);
-	const iconClasses = "mr-4 w-5 h-5";
+  const currentSubscription = useSelector(selectCurrentSubscription);
+  const currentMember = useSelector(selectCurrentMember);
+  const currentTeam = useSelector(selectCurrentTeam);
 
-	return (
-		<StyledDialog
-			onCloseDialog={onCloseDialog}
-			open={open}
-			title={currentTeam.name}
-			fullscreen={false}
-		>
-			<Link to="/">
-				<MobileMenuButton full onClick={onCloseDialog}>
-					<div className="flex items-center text-gray-700 dark:text-dark-gray-200">
-						<DashboardIcon className={iconClasses} />
-						Dashboard
-					</div>
-				</MobileMenuButton>
-			</Link>
+  const iconClasses = 'mr-5 w-5 h-5';
 
-			<Link to="/binders">
-				<MobileMenuButton full onClick={onCloseDialog}>
-					<div className="flex items-center text-gray-700 dark:text-dark-gray-200">
-						<BinderIcon className={iconClasses} />
-						Binders
-					</div>
-				</MobileMenuButton>
-			</Link>
+  const shouldShowDividers =
+    currentMember?.can(VIEW_ROLES) || currentMember?.can(MANAGE_BILLING);
 
-			<Link to="/songs">
-				<MobileMenuButton full onClick={onCloseDialog}>
-					<div className="flex items-center text-gray-700 dark:text-dark-gray-200">
-						<MusicNoteIcon className={iconClasses} />
-						Songs
-					</div>
-				</MobileMenuButton>
-			</Link>
+  return (
+    <StyledDialog
+      onCloseDialog={onCloseDialog}
+      open={open}
+      title={currentTeam.name}
+      fullscreen={true}
+    >
+      <Link to="/">
+        <MobileMenuButton full onClick={onCloseDialog} size="none">
+          <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+            <DashboardIcon className={iconClasses} />
+            Dashboard
+          </div>
+        </MobileMenuButton>
+      </Link>
 
-			<Link to="/sets">
-				<MobileMenuButton full onClick={onCloseDialog}>
-					<div className="flex items-center text-gray-700 dark:text-dark-gray-200">
-						<PlaylistIcon className={iconClasses} />
-						Sets
-					</div>
-				</MobileMenuButton>
-			</Link>
+      <Link to="/binders">
+        <MobileMenuButton full onClick={onCloseDialog} size="none">
+          <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+            <BinderIcon className={iconClasses} />
+            Binders
+          </div>
+        </MobileMenuButton>
+      </Link>
 
-			<Link to="/members">
-				<MobileMenuButton full onClick={onCloseDialog}>
-					<div className="flex items-center text-gray-700 dark:text-dark-gray-200">
-						<UserGroupIcon className={iconClasses} />
-						Team members
-					</div>
-				</MobileMenuButton>
-			</Link>
+      <Link to="/songs">
+        <MobileMenuButton full onClick={onCloseDialog} size="none">
+          <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+            <MusicNoteIcon className={iconClasses} />
+            Songs
+          </div>
+        </MobileMenuButton>
+      </Link>
 
-			{currentSubscription?.isPro && currentMember?.can(VIEW_EVENTS) && (
-				<Link to="/calendar">
-					<MobileMenuButton full onClick={onCloseDialog}>
-						<div className="flex items-center text-gray-700 dark:text-dark-gray-200">
-							<CalendarIcon className={iconClasses} />
-							Calendar
-						</div>
-					</MobileMenuButton>
-				</Link>
-			)}
-			<Link to="/login/teams">
-				<MobileMenuButton full onClick={onCloseDialog}>
-					<div className="flex items-center text-gray-700 dark:text-dark-gray-200">
-						<SwitchHorizontalIcon className={iconClasses} />
-						Switch teams
-					</div>
-				</MobileMenuButton>
-			</Link>
-		</StyledDialog>
-	);
+      <Link to="/sets">
+        <MobileMenuButton full onClick={onCloseDialog} size="none">
+          <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+            <PlaylistIcon className={iconClasses} />
+            Sets
+          </div>
+        </MobileMenuButton>
+      </Link>
+
+      <Link to="/members">
+        <MobileMenuButton full onClick={onCloseDialog} size="none">
+          <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+            <UserGroupIcon className={iconClasses} />
+            Team members
+          </div>
+        </MobileMenuButton>
+      </Link>
+
+      {currentSubscription?.isPro && currentMember?.can(VIEW_EVENTS) && (
+        <Link to="/calendar">
+          <MobileMenuButton full onClick={onCloseDialog} size="none">
+            <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+              <CalendarIcon className={iconClasses} />
+              Calendar
+            </div>
+          </MobileMenuButton>
+        </Link>
+      )}
+
+      {shouldShowDividers && (
+        <div className="h-px pt-4 mb-4 border-b dark:border-dark-gray-400" />
+      )}
+      {currentMember?.can(VIEW_ROLES) && (
+        <Link to="/permissions">
+          <MobileMenuButton full onClick={onCloseDialog} size="none">
+            <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+              <LockClosedIcon className={iconClasses} />
+              Permissions
+            </div>
+          </MobileMenuButton>
+        </Link>
+      )}
+      {currentMember?.can(MANAGE_BILLING) && (
+        <Link to="/billing">
+          <MobileMenuButton full onClick={onCloseDialog} size="none">
+            <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+              <CreditCardIcon className={iconClasses} />
+              Billing
+            </div>
+          </MobileMenuButton>
+        </Link>
+      )}
+      {shouldShowDividers && (
+        <div className="h-px pt-4 mb-4 border-b dark:border-dark-gray-400" />
+      )}
+      <Link to="/login/teams">
+        <MobileMenuButton full onClick={onCloseDialog} size="none">
+          <div className="flex items-center text-gray-700 dark:text-dark-gray-200">
+            <SwitchHorizontalIcon className={iconClasses} />
+            Switch teams
+          </div>
+        </MobileMenuButton>
+      </Link>
+    </StyledDialog>
+  );
 }
