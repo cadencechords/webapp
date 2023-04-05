a466a08edeac2b143bfe8db8e59b6a3ee59b2987
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2023-04-05 09:22:32 -0400

    redirect to correct page

--- src/pages/PcoRedirectPage.js
+++ src/pages/PcoRedirectPage.js
@@ -1,26 +1,26 @@
-import PlanningCenterApi from "../api/PlanningCenterApi";
-import { reportError } from "../utils/error";
-import { useEffect } from "react";
-import { useHistory } from "react-router";
-import useQuery from "../hooks/useQuery";
+import PlanningCenterApi from '../api/PlanningCenterApi';
+import { reportError } from '../utils/error';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import useQuery from '../hooks/useQuery';
 
 export default function PcoRedirectPage() {
-	const code = useQuery().get("code");
-	const router = useHistory();
+  const code = useQuery().get('code');
+  const router = useHistory();
 
-	useEffect(() => {
-		async function authorize() {
-			try {
-				await PlanningCenterApi.authorize(code);
-				router.push("/import/pco/songs");
-			} catch (error) {
-				reportError(error);
-			}
-		}
-		if (code) {
-			authorize();
-		}
-	}, [code, router]);
+  useEffect(() => {
+    async function authorize() {
+      try {
+        await PlanningCenterApi.authorize(code);
+        router.push('/import/planning-center');
+      } catch (error) {
+        reportError(error);
+      }
+    }
+    if (code) {
+      authorize();
+    }
+  }, [code, router]);
 
-	return <>Connecting to your account...</>;
+  return <>Connecting to your account...</>;
 }
