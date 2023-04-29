a5d1085c47fba20b142d1978866edb2c8bbd36e6
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2023-04-29 14:01:46 -0400

    update mobile nav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -4,8 +4,10 @@ import MobileNavLink from './MobileNavLink';
 import SearchIcon from '@heroicons/react/solid/SearchIcon';
 import UserIcon from '@heroicons/react/solid/UserIcon';
 import { useState } from 'react';
+import MusicNoteIcon from '@heroicons/react/solid/MusicNoteIcon';
+import PlaylistIcon from '../icons/PlaylistIcon';
 
-export default function MobileNavbar() {
+export default function MobileNav() {
   const [showMenuDialog, setShowMenuDialog] = useState(false);
 
   const iconClasses = 'h-6 w-6';
@@ -13,16 +15,29 @@ export default function MobileNavbar() {
     <>
       <div className="fixed bottom-0 z-40 flex w-full py-1 bg-white border-t dark:bg-dark-gray-800 dark:border-0 md:hidden">
         <MobileNavLink
-          icon={<SearchIcon className={iconClasses} />}
-          to="/search"
+          icon={<MusicNoteIcon className={iconClasses} />}
+          to="/songs"
+          text="Songs"
+        />
+        <MobileNavLink
+          icon={<PlaylistIcon className="w-7 h-7" />}
+          to="/sets"
+          text="Sets"
         />
         <MobileNavLink
           icon={<MenuIcon className={iconClasses} />}
           onClick={() => setShowMenuDialog(true)}
+          text="Menu"
+        />
+        <MobileNavLink
+          icon={<SearchIcon className={iconClasses} />}
+          to="/search"
+          text="Search"
         />
         <MobileNavLink
           icon={<UserIcon className={iconClasses} />}
           to="/account"
+          text="Account"
         />
       </div>
       <AppMenu
--- src/components/MobileNavLink.js
+++ src/components/MobileNavLink.js
@@ -1,40 +1,40 @@
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link, useRouteMatch } from 'react-router-dom';
 
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 export default function MobileNavLink({ text, to, icon, onClick }) {
-	let isCurrentRoute = useRouteMatch({
-		path: to,
-	});
-	if (to) {
-		return (
-			<Link
-				to={to}
-				className={
-					`flex-center flex-col flex-grow py-2 font-semibold transition-all ` +
-					`${
-						isCurrentRoute
-							? " text-blue-700 dark:text-dark-blue "
-							: "hover:text-gray-700 text-gray-500 dark:text-dark-gray-200"
-					} `
-				}
-			>
-				<span>{icon}</span> <span className="text-sm">{text}</span>
-			</Link>
-		);
-	} else {
-		return (
-			<button
-				onClick={onClick}
-				className="focus:outline-none outline-none py-2 flex-center flex-grow flex-col rounded font-semibold transition-all hover:text-gray-700 text-gray-500 dark:text-dark-gray-200"
-			>
-				<span>{icon}</span>
-				<span className="text-sm">{text}</span>
-			</button>
-		);
-	}
+  let isCurrentRoute = useRouteMatch({
+    path: to,
+  });
+  if (to) {
+    return (
+      <Link
+        to={to}
+        className={
+          `flex-center flex-col flex-grow font-semibold transition-all ` +
+          `${
+            isCurrentRoute
+              ? ' text-blue-700 dark:text-dark-blue '
+              : 'hover:text-gray-700 text-gray-500 dark:text-dark-gray-200'
+          } `
+        }
+      >
+        <span>{icon}</span> <span className="text-xs font-normal">{text}</span>
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        onClick={onClick}
+        className="flex-col flex-grow py-1 font-semibold text-gray-500 transition-all rounded outline-none focus:outline-none flex-center hover:text-gray-700 dark:text-dark-gray-200"
+      >
+        <span>{icon}</span>
+        <span className="text-xs font-normal">{text}</span>
+      </button>
+    );
+  }
 }
 
 MobileNavLink.propTypes = {
-	text: PropTypes.string,
+  text: PropTypes.string,
 };
