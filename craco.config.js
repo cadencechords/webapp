829de4465b55a5e360fd710c0816035f68103a2f
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2023-05-12 06:58:49 -0400

    upgrade chord-kit to resolve chordpro error

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+  webpack: {
+    configure: webpackConfig => {
+      webpackConfig.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      });
+      return webpackConfig;
     },
   },
-}
+  style: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+};
