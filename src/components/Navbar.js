bf8a52a39dc6475741ebc1639fb769a2970b7a69
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2024-11-08 17:00:38 -0500

    Show upgrade button in top nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,35 @@
+import { useSelector } from 'react-redux';
+import useCreateCustomerPortalSession from '../hooks/api/useCreateCustomerProtalSession';
+import useSubscription from '../hooks/api/useSubscription';
+import { selectCurrentMember } from '../store/authSlice';
 import AccountOptionsPopover from './AccountOptionsPopover';
+import Button from './Button';
 import FeedbackPopover from './FeedbackPopover';
 import SearchBar from './SearchBar';
-// import SupportButton from "./SupportButton";
+import { MANAGE_BILLING } from '../utils/constants';
 
 export default function Navbar() {
+  const { data: subscription } = useSubscription();
+  const { isLoading: isCreatingSession, run: createCustomerPortalSession } =
+    useCreateCustomerPortalSession();
+  const currentMember = useSelector(selectCurrentMember);
+  const showUpgradeButton =
+    currentMember.can(MANAGE_BILLING) && subscription?.plan_name === 'Starter';
+
   return (
     <nav className="items-center justify-between hidden h-16 px-4 border-b md:flex md:ml-14 lg:ml-56 dark:border-dark-gray-700">
       <SearchBar />
       <span className="flex-center">
-        {/* <SupportButton /> */}
+        {showUpgradeButton && (
+          <Button
+            color="purple"
+            className="w-24 mr-8"
+            onClick={createCustomerPortalSession}
+            loading={isCreatingSession}
+          >
+            Upgrade
+          </Button>
+        )}
         <FeedbackPopover />
         <AccountOptionsPopover />
       </span>
